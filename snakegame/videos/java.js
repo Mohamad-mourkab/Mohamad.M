d7a659c88b1aa98ce5fc06acde8d4cc0ada6c2db
Mohamad-mourkab <145436414+Mohamad-mourkab@users.noreply.github.com>
2024-01-08 11:23:52 +0100

    java

--- snakegame/videos/java.js
+++ snakegame/videos/java.js
@@ -1,1 +1,52 @@
 const board = document.getElementById('game-board');
+
+//game var
+
+let snake = [{ x: 10, y: 10 }]
+let food = generatefood();
+const gridSize = 20
+
+function draw(){
+    board.innerHTML = "";
+    drawSnake();
+    drawFood();
+}
+// snake
+function drawSnake(){
+    snake.forEach((segment) => {
+        const snakeElement = createGameElement('div',
+         'snake');
+         setPosition(snakeElement, segment);
+         board.appendChild(snakeElement);
+    });
+}
+
+function createGameElement(tag, className) {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+//position of the snake or food
+
+function setPosition(element, position){
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
+
+}
+
+//food
+function drawFood(){
+    const foodElement = createGameElement('div', 'food');
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+}
+
+function generatefood() {
+    const x = Math.floor(Math.random() * gridSize) + 1;
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    return( x, y )
+}
+
+//test
+draw();
